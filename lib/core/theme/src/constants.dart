9c6278aded391ddf60f56c13a51d0d338d39199b
Oganes <debel.ovik@yandex.ru>
2023-05-20 23:48:41 +0300

    Added themes and consts

---
+++ lib/core/theme/src/constants.dart
@@ -0,0 +1,29 @@
+part of 'package:socialsquad/core/theme/theme.dart';
+
+abstract class Const {
+  static const fontSize10 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 10);
+  static const fontSize11 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 11);
+  static const fontSize20 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 20);
+  static const fontSize12 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 12);
+  static const fontSize15 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 15);
+  static const fontSize16 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 16);
+  static const fontSize14 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 14);
+  static const fontSize24 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 24);
+  static const fontSize32 =
+      TextStyle(fontWeight: FontWeight.w400, fontSize: 32);
+  static const darkBackground = Color(0xFF1C1C1E);
+  static const white = Colors.white;
+  static const phiol = Color(0xFF5A33DB);
+  static const disabledText = Color(0xFF747272);
+  static const backroundDay = Color(0xFF3D3D3F);
+  static const darkText = Color(0xFF8E8E93);
+  static const errorColor = Color(0xFFB3261E);
+}
